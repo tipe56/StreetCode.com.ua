cddeccef09f44238fa1ed7313900abec168cfdf1
Siarhei Ramaniuk <s.m.ramaniuk@gmail.com>
2023-11-23 15:36:27 +0100

    Add person images, mockData and historicalPerson struct

---
+++ Streetcode.com.ua/PersonGridView.swift
@@ -0,0 +1,26 @@
+//
+//  PersonGridView.swift
+//  Streetcode.com.ua
+//
+//  Created by Siarhei Ramaniuk on 23.11.23.
+//
+
+import SwiftUI
+
+struct PersonGridView: View {
+    
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        Text("")
+    }
+}
+
+struct PersonGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonGridView()
+    }
+}
