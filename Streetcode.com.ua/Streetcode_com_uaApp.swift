0d777fc45586775642532583c2c123480badf405
Siarhei Ramaniuk <s.m.ramaniuk@gmail.com>
2023-11-23 15:06:36 +0100

    Initial Commit

---
+++ Streetcode.com.ua/Streetcode_com_uaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Streetcode_com_uaApp.swift
+//  Streetcode.com.ua
+//
+//  Created by Siarhei Ramaniuk on 23.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Streetcode_com_uaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
